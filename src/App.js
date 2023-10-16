b5fafc8457da3cb66a7f4a4ed63abdd797a24831
YourLine <103743058+ward-yourline@users.noreply.github.com>
2023-10-16 23:30:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
-// import React from 'react';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import HomePage from './components/pages/HomePage';
-// import SignInPage from './components/pages/SignInPage';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//         <Route path="/signin" element={<SignInPage />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import SignInPage from './components/pages/SignInPage';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
   return (
-    <div>
-      <h1>Simple React Counter</h1>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/signin" element={<SignInPage />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
