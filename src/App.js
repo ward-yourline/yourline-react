a2aed66d7ed57e8bfbb9af0d97c8faea5caefcb0
YourLine <103743058+ward-yourline@users.noreply.github.com>
2023-10-16 23:26:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from './components/pages/HomePage';
-import SignInPage from './components/pages/SignInPage';
+// import React from 'react';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import HomePage from './components/pages/HomePage';
+// import SignInPage from './components/pages/SignInPage';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/signin" element={<SignInPage />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signin" element={<SignInPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>Simple React Counter</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
   );
 }
 
