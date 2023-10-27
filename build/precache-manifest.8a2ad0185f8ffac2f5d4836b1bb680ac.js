0305ca8bca1b2ac25beec541d5330289ac8a5e0e
YourLine <103743058+ward-yourline@users.noreply.github.com>
2023-10-27 18:19:01 +0100

    build files

---
+++ build/precache-manifest.8a2ad0185f8ffac2f5d4836b1bb680ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b53b6b9214f7eb51ab4f71c25500be",
+    "url": "/yourline-react/index.html"
+  },
+  {
+    "revision": "0f74a5b52793d725b4c9",
+    "url": "/yourline-react/static/css/main.2ff4725b.chunk.css"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/yourline-react/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "cecf616dbfc830b280dc",
+    "url": "/yourline-react/static/js/3.d7f7bd74.chunk.js"
+  },
+  {
+    "revision": "0f74a5b52793d725b4c9",
+    "url": "/yourline-react/static/js/main.54f8039e.chunk.js"
+  },
+  {
+    "revision": "0791e257618d9f76e329",
+    "url": "/yourline-react/static/js/runtime~main.0e59c073.js"
+  }
+]);
