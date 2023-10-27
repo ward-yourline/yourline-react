015da3b02764eaefd6f0efabb637f0ce7a0e8ecd
YourLine <103743058+ward-yourline@users.noreply.github.com>
2023-10-27 18:12:06 +0100

    UI tweaks and copy for disclaimer

---
+++ src/components/layout/Separator.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Separator = () => {
+    return <hr style={{opacity: 0.2}} />
+}
+
+export default Separator;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HomePage.css'; // Import your styles
+import Separator from '../layout/Separator';
 import Padding from '../layout/Padding';
 import TextLabel, { TextLabelType, TextLabelAlignment } from '../text/TextLabel';
 import { Link } from 'react-router-dom'; // Import Link from React Router
@@ -22,8 +23,7 @@ const HomePage = () => {
             <div className="logo-container">
                 <img className="logo" src="assets/yl_vertical_logo.svg" alt="YourLine Logo" />
             </div>
-            <hr className="nav-separator" />
-
+            <Separator />
             <br />
 
             <div className="section-container">
@@ -44,9 +44,25 @@ const HomePage = () => {
                     <TextLabel type={TextLabelType.h3} alignment={TextLabelAlignment.center} text="Empowering Global Connectivity: YourLine's Seamless Solution" />
                     <TextLabel type={TextLabelType.p} fontSize="18px" alignment={TextLabelAlignment.center} text="In a world where connections matter more than ever, YourLine simplifies life's complexities.
                         Our revolutionary platform streamlines online experiences for individuals, businesses, and communities.
-                        From persoTextLabelnal networks to seamless business interactions, YourLine bridges gaps and empowers connections." />
+                        From personal networks to seamless business interactions, YourLine bridges gaps and empowers connections." />
                 </div>
                 <Padding flex={1} />
+            </div>
+            <br></br>
+            <div className='beta-div'>
+                <mark><strong>BETA</strong>
+                </mark>
+            </div>
+            <div className='beta-div'>
+                <p>
+                    This is a <em><mark>BETA</mark></em> build of the business module. You will be able to build a business for products and services, or be a customer, or both.
+                </p>
+                <p>
+                    <em>We are constantly improving the service but please be aware that there may still be bugs in the system.</em>
+                </p>
+                <p>
+                    If you have any queries, or would like to report any bugs, send an email <a href="mailto:ward.yourline@gmail.com?subject = Feedback">here</a>
+                </p>
             </div>
         </div>
     );
